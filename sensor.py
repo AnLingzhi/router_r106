8401ebf96700ec2a1ff57e1fa647978be9f69f87
anlingzhi <anlingzhi@live.com>
2025-03-09 22:41:18 +0800

    拆分多sersor

--- sensor.py
+++ sensor.py
@@ -1,15 +1,13 @@
 import logging
 import requests
-import json
 import hmac
 import hashlib
+import time
 from datetime import timedelta, datetime
 import voluptuous as vol
-import time
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
-from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.util import Throttle
 
@@ -24,91 +22,22 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional('testurl', default=DEFAULT_TESTURL): cv.string,
 })
 
-# "device_uptime" : "300",
-# "device_battery_level" : "3",
-# "device_battery_level_percent" : "59",
-# "device_battery_charge_status" : "charging"
-# "device_battery_temperature" : "42",
-# "mnet_sim_status" : "ready",
-# "mnet_sysmode" : "nr5g",
-# "mnet_sig_level" : "great",
-# "mnet_roam_status" : "off",
-# "mnet_operator_name" : "中国移动",
-# "rt_internet_mode" : "mobile_data",
-# "rt_eth_conn_info" : "",
-# "dialup_dial_status" : "connected",
-# "wifi_work_status" : "open",
-# "sms_unread_count" : "0",
-# "fota_curr_istatus" : "no new version",
 
+class RouterAPI:
+    """路由器 API 交互类，管理登录和数据获取。"""
 
-# 获取路由器状态信息
-class RouterR106(Entity):
     def __init__(self, username, password):
-        self._name = "R106 Status Sensor"
         self._username = username
         self._password = password
         self._session = requests.Session()
-        self.device_uptime = None
-        self.device_battery_level = None
-        self.device_battery_level_percent = None
-        self.device_battery_charge_status = None
-        self.device_battery_temperature = None
-        self.mnet_sim_status = None
-        self.mnet_sysmode = None
-        self.mnet_sig_level = None
-        self.mnet_roam_status = None
-        self.mnet_operator_name = None
-        self.rt_internet_mode = None
-        self.rt_eth_conn_info = None
-        self.dialup_dial_status = None
-        self.wifi_work_status = None
-        self.sms_unread_count = None
-        self.fota_curr_istatus = None
-        self._unique_id = f"r106_status_{username}"  # 生成唯一标识符
-
-    @property
-    def unique_id(self):
-        return self._unique_id
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        return float(self.device_battery_level_percent)
-
-    @property
-    def unit_of_measurement(self):
-        return "%"
-
-    @property
-    def extra_state_attributes(self):
-        return {
-            'device_uptime': self.device_uptime,
-            'device_battery_level': self.device_battery_level,
-            'device_battery_level_percent': self.device_battery_level_percent,
-            'device_battery_charge_status': self.device_battery_charge_status,
-            'device_battery_temperature': self.device_battery_temperature,
-            'mnet_sim_status': self.mnet_sim_status,
-            'mnet_sysmode': self.mnet_sysmode,
-            'mnet_sig_level': self.mnet_sig_level,
-            'mnet_roam_status': self.mnet_roam_status,
-            'mnet_operator_name': self.mnet_operator_name,
-            'rt_internet_mode': self.rt_internet_mode,
-            'rt_eth_conn_info': self.rt_eth_conn_info,
-            'dialup_dial_status': self.dialup_dial_status,
-            'wifi_work_status': self.wifi_work_status,
-            'sms_unread_count': self.sms_unread_count,
-            'fota_curr_istatus': self.fota_curr_istatus
-        }
+        self.logged_in = False
 
     def hex_hmac_md5(self, key, data):
-        hmac_md5 = hmac.new(key.encode(), data.encode(), hashlib.md5)
-        return hmac_md5.hexdigest()
+        """HMAC-MD5 加密"""
+        return hmac.new(key.encode(), data.encode(), hashlib.md5).hexdigest()
 
     def login(self):
+        """登录路由器"""
         url = "http://192.168.1.1/goform/login"
         headers = {
             "Content-Type": "application/json",
@@ -117,24 +46,31 @@ class RouterR106(Entity):
         }
         username_hmac = self.hex_hmac_md5("0123456789", self._username)
         password_hmac = self.hex_hmac_md5("0123456789", self._password)
-        data = {
-            "username": username_hmac,
-            "password": password_hmac
-        }
-        response = self._session.post(url, headers=headers, json=data)
-        response.raise_for_status()
-        self._session.cookies = response.cookies
-    
-    def check_battery_status(self):
+        data = {"username": username_hmac, "password": password_hmac}
+
+        try:
+            response = self._session.post(url, headers=headers, json=data)
+            response.raise_for_status()
+            self._session.cookies = response.cookies
+            self.logged_in = True
+            _LOGGER.info("Router login successful")
+        except requests.exceptions.RequestException as e:
+            _LOGGER.error("Router login failed: %s", e)
+            self.logged_in = False
+
+    def get_status(self):
+        """获取路由器状态信息"""
+        if not self.logged_in:
+            self.login()
+        if not self.logged_in:  # 再次检查是否登录成功
+            return None
+
         url = "http://192.168.1.1/action/get_mgdb_params"
         data = {
             "keys": [
                 "device_uptime",
-                "device_battery_level",
                 "device_battery_level_percent",
-                "device_battery_charge_status",
                 "device_battery_temperature",
-                "mnet_sim_status",
                 "mnet_sysmode",
                 "mnet_sig_level",
                 "mnet_roam_status",
@@ -147,108 +83,157 @@ class RouterR106(Entity):
                 "fota_curr_istatus"
             ]
         }
-        
-        response = self._session.post(url, json=data)
-        response.raise_for_status()
-        result = response.json()
-        self.device_uptime = result["data"].get("device_uptime")
-        self.device_battery_level = result["data"].get("device_battery_level")
-        self.device_battery_level_percent = result["data"].get("device_battery_level_percent")
-        self.device_battery_charge_status = result["data"].get("device_battery_charge_status")
-        self.device_battery_temperature = result["data"].get("device_battery_temperature")
-        self.mnet_sim_status = result["data"].get("mnet_sim_status")
-        self.mnet_sysmode = result["data"].get("mnet_sysmode")
-        self.mnet_sig_level = result["data"].get("mnet_sig_level")
-        self.mnet_roam_status = result["data"].get("mnet_roam_status")
-        self.mnet_operator_name = result["data"].get("mnet_operator_name")
-        self.rt_internet_mode = result["data"].get("rt_internet_mode")
-        self.rt_eth_conn_info = result["data"].get("rt_eth_conn_info")
-        self.dialup_dial_status = result["data"].get("dialup_dial_status")
-        self.wifi_work_status = result["data"].get("wifi_work_status")
-        self.sms_unread_count = result["data"].get("sms_unread_count")
-        self.fota_curr_istatus = result["data"].get("fota_curr_istatus")
-        _LOGGER.info("Battery level: %s, Battery level percent: %s, Charge status: %s, Temperature: %s", self.device_battery_level, self.device_battery_level_percent, self.device_battery_charge_status, self.device_battery_temperature)
-        _LOGGER.info("SIM status: %s, System mode: %s, Signal level: %s, Roaming status: %s, Operator name: %s, Internet mode: %s, Ethernet connection info: %s, Dialup dial status: %s, WiFi work status: %s, Unread SMS count: %s, Firmware update status: %s", self.mnet_sim_status, self.mnet_sysmode, self.mnet_sig_level, self.mnet_roam_status, self.mnet_operator_name, self.rt_internet_mode, self.rt_eth_conn_info, self.dialup_dial_status, self.wifi_work_status, self.sms_unread_count, self.fota_curr_istatus)
 
-    def reboot_router(self):
-        url = "http://192.168.1.1/action/reboot"
         try:
-            response = self._session.post(url)
+            response = self._session.post(url, json=data)
             response.raise_for_status()
-            _LOGGER.info("Router rebooted successfully")
+            return response.json().get("data", {})
         except requests.exceptions.RequestException as e:
-            _LOGGER.error("Error rebooting router: %s", e)
+            _LOGGER.error("Failed to fetch router status: %s", e)
+            return None
+
+
+class RouterBatterySensor(SensorEntity):
+    """电池百分比传感器"""
+
+    def __init__(self, router_api):
+        self._router_api = router_api
+        self._attr_name = "Router Battery Level"
+        self._attr_unit_of_measurement = "%"
+        self._attr_unique_id = "router_battery_level"
+        self._state = None
+
+    @property
+    def state(self):
+        return self._state
 
     @Throttle(SCAN_INTERVAL)
     def update(self):
-        self.login()  # 每次更新时都进行登录
-        self.check_battery_status()
+        status = self._router_api.get_status()
+        if status:
+            self._state = status.get("device_battery_level_percent")
 
-class NetworkProber(Entity):
-    def __init__(self, testurl):
-        self._name = "R106 Network Prober"
-        self.testurl = testurl
-        self._session = requests.Session()
-        self._status = None
-        self._last_probe_time = None
-        self._last_probe_duration = None
-        self._last_probe_result = 0
-        self._last_probe_error = None
-        self._last_successful_probe_time = None
-        self._last_failed_probe_time = None
-        self._unique_id = f"r106_network_{testurl}"  # 生成唯一标识符
 
-    @property
-    def unique_id(self):
-        return self._unique_id
+class RouterBatteryTempSensor(SensorEntity):
+    """电池温度传感器"""
 
-    @property
-    def name(self):
-        return self._name
-    
+    def __init__(self, router_api):
+        self._router_api = router_api
+        self._attr_name = "Router Battery Temperature"
+        self._attr_unit_of_measurement = "°C"
+        self._attr_unique_id = "router_battery_temp"
+        self._state = None
+
     @property
     def state(self):
-        return self._last_probe_result
-    
+        return self._state
+
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        status = self._router_api.get_status()
+        if status:
+            self._state = status.get("device_battery_temperature")
+
+
+class RouterNetworkModeSensor(SensorEntity):
+    """网络模式传感器"""
+
+    def __init__(self, router_api):
+        self._router_api = router_api
+        self._attr_name = "Router Network Mode"
+        self._attr_unique_id = "router_network_mode"
+        self._state = None
+
+    @property
+    def state(self):
+        return self._state
+
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        status = self._router_api.get_status()
+        if status:
+            self._state = status.get("mnet_sysmode")
+
+
+class RouterExtraAttributesSensor(SensorEntity):
+    """存储额外信息的传感器"""
+
+    def __init__(self, router_api):
+        self._router_api = router_api
+        self._attr_name = "Router Extra Attributes"
+        self._attr_unique_id = "router_extra_attributes"
+        self._attributes = {}
+
+    @property
+    def state(self):
+        return self._attributes.get("device_uptime", "Unknown")  # 显示设备运行时间
+
+    @property
+    def extra_state_attributes(self):
+        return self._attributes
+
+    @Throttle(SCAN_INTERVAL)
+    def update(self):
+        status = self._router_api.get_status()
+        if status:
+            self._attributes = status
+
+
+class NetworkProber(SensorEntity):
+    """网络连通性探测传感器"""
+
+    def __init__(self, testurl):
+        self.testurl = testurl
+        self._attr_name = "Network Connectivity"
+        self._attr_unique_id = "network_prober"
+        self._state = 0
+        self._last_probe_time = None
+        self._last_probe_duration = None
+        self._last_probe_error = None
+
+    @property
+    def state(self):
+        return self._state
+
     @property
     def extra_state_attributes(self):
         return {
             "last_probe_time": self._last_probe_time,
-            "last_probe_result": self._last_probe_result,
             "last_probe_duration": self._last_probe_duration,
             "last_probe_error": self._last_probe_error,
-            "last_successful_probe_time": self._last_successful_probe_time,
-            "last_failed_probe_time": self._last_failed_probe_time,
         }
 
-    def probe(self):
-        start_time = time.time()
-        self._last_probe_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        try:
-            # 添加https头
-            url = f"https://{self.testurl}"
-            response = self._session.get(url, timeout=5)
-            response.raise_for_status()
-            self._last_probe_result = 1
-            self._last_probe_error = None
-            self._last_successful_probe_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        except requests.exceptions.RequestException as e:
-            self._last_probe_result = 0
-            self._last_probe_error = str(e)
-            self._last_failed_probe_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            _LOGGER.error("Error probing network: %s", e)
-        self._last_probe_duration = time.time() - start_time
-        return self._last_probe_result   
-    
     @Throttle(SCAN_INTERVAL)
     def update(self):
-        self.probe()
+        start_time = time.time()
+        self._last_probe_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        try:
+            url = f"https://{self.testurl}"
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()
+            self._state = 1
+            self._last_probe_error = None
+        except requests.exceptions.RequestException as e:
+            self._state = 0
+            self._last_probe_error = str(e)
+
+        self._last_probe_duration = round(time.time() - start_time, 2)
+
+
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    username = config.get(CONF_USERNAME)
-    password = config.get(CONF_PASSWORD)
-    router = RouterR106(username, password)
-    
+    username = config[CONF_USERNAME]
+    password = config[CONF_PASSWORD]
     testurl = config.get('testurl')
-    networkprober = NetworkProber(testurl)
-    
-    add_entities([router, networkprober], True)
+
+    router_api = RouterAPI(username, password)
+
+    sensors = [
+        RouterBatterySensor(router_api),
+        RouterBatteryTempSensor(router_api),
+        RouterNetworkModeSensor(router_api),
+        RouterExtraAttributesSensor(router_api),  # 额外属性传感器
+        NetworkProber(testurl),
+    ]
+
+    add_entities(sensors, True)
