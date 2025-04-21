86c39eb7d0a33c2391b4f6db9240a6c9ba52ca0f
An Lingzhi <anlingzhi@live.com>
2025-04-21 14:19:20 +0800

    feat(router_r106): 支持通过entity_id指定重启路由器
    添加对entity_id参数的支持，允许用户指定要重启的路由器。如果未提供entity_id，则重启所有路由器。

--- __init__.py
+++ __init__.py
@@ -6,10 +6,21 @@ DOMAIN = "router_r106"
 
 async def async_setup(hass: HomeAssistant, config: dict):
     async def handle_reboot(call):
-        for platform in async_get_platforms(hass, DOMAIN):
-            for entity in platform.entities.values():
-                if hasattr(entity, 'reboot_router'):
-                    await hass.async_add_executor_job(entity.reboot_router)
+        entity_id = call.data.get('entity_id')
+        
+        # 如果指定了entity_id，只重启指定的路由器
+        if entity_id:
+            for platform in async_get_platforms(hass, DOMAIN):
+                for entity_obj in platform.entities.values():
+                    if entity_obj.entity_id == entity_id and hasattr(entity_obj, 'reboot_router'):
+                        await hass.async_add_executor_job(entity_obj.reboot_router)
+                        return
+        # 如果没有指定entity_id，重启所有路由器
+        else:
+            for platform in async_get_platforms(hass, DOMAIN):
+                for entity_obj in platform.entities.values():
+                    if hasattr(entity_obj, 'reboot_router'):
+                        await hass.async_add_executor_job(entity_obj.reboot_router)
 
     hass.services.async_register(DOMAIN, "reboot", handle_reboot)
 
